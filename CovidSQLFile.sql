179bce31b0c31e60d390b7e366899ff50094e45a
masondelrio <59667488+masondelrio@users.noreply.github.com>
2022-07-12 17:16:05 -0700

    Delete CovidSQLFile.sql

--- CovidSQLFile.sql
+++
@@ -1,9 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT DISTINCT TOP (1000) [continent]
-      ,[location]
-      ,[date]
-      ,[population]
-      ,[new_vaccinations]
-      ,[RollingPeopleVaccinated]
-  FROM [PortfolioProject].[dbo].[PercentPopulationVaccinated]
-  order by 1,2,3
